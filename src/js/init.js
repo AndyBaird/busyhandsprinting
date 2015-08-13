55c980e833aa2e80b455e915af46b0f66268e2a3
Andy Baird <Andy.Baird@icloud.com>
2015-08-13 10:44:12 -0600

    initial commit with previous file batch

---
+++ src/js/init.js
@@ -0,0 +1,7 @@
+// TODO: put initialization logic here
+var $ = require('jquery'); 
+var slick = require('../../bower_components/slick.js/slick/slick.js');
+var home = require('./home');
+
+console.log(home);
+home();
