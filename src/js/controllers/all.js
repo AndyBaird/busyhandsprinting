7db95028c836d48deb5706af0b1e80e5fb8bbf06
Andy Baird <Andy.Baird@icloud.com>
2015-09-01 13:38:58 -0600

    added new images

--- src/js/controllers/all.js
+++ src/js/controllers/all.js
@@ -3,3 +3,8 @@ var $ = require('jquery');
 $(window).load(function() {
     $('.banner-image-container').css("visibility", "visible");
 });
+
+//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
+if (!("ontouchstart" in document.documentElement)) {
+document.documentElement.className += " no-touch";
+}
